8e50610c6dbd8802eeb50bfc0b9210b710ddc11c
Soheil Alizadeh <xsoheilalizadeh@gmail.com>
2019-10-17 19:06:45 +0330

    Update travis

--- install.dotnet.sh
+++
@@ -1,6 +0,0 @@
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-sdk-3.0
