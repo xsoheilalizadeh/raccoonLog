2ee27b391c534b572890e39dcb108098ce339458
Soheil Alizadeh <xsoheilalizadeh@gmail.com>
2019-10-18 20:21:54 +0330

    Add Response log handler tests

---
+++ test/raccoonLog.Tests/Handlers/DefaultHttpResponseLogHandlerTests.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using raccoonLog.Http;
+using Xunit;
+
+namespace raccoonLog.Tests.Handlers
+{
+    public class DefaultHttpResponseLogHandlerTests
+    {
+        private Mock<IHttpLogMessageFactory> _logMessageFactor;
+        private Mock<IHttpResponseLogBodyHandler> _bodyHandler;
+
+        public DefaultHttpResponseLogHandlerTests()
+        {
+            _logMessageFactor = new Mock<IHttpLogMessageFactory>();
+            _bodyHandler = new Mock<IHttpResponseLogBodyHandler>();
+        }
+
+
+        [Fact]
+        public async Task HandleThrowsNullReferenceExceptionOnNullResponse()
+        {
+            // arrange
+            var body = new MemoryStream();
+            var handler = CreateHandler();
+
+            // act and assert
+            await Assert.ThrowsAsync<NullReferenceException>(() => handler.Handle(null, body));
+        }
+
+        
+        [Fact]
+        public async Task HandleThrowsNullReferenceExceptionOnNullBody()
+        {
+            // arrange
+            var context = new DefaultHttpContext();
+            var handler = CreateHandler();
+
+            // act and assert
+            await Assert.ThrowsAsync<NullReferenceException>(() => handler.Handle(context.Response,null));
+        }
+
+        private DefaultHttpResponseLogHandler CreateHandler()
+        {
+            return new DefaultHttpResponseLogHandler(
+                 _logMessageFactor.Object,
+                 _bodyHandler.Object
+            );
+        }
+    }
+}
