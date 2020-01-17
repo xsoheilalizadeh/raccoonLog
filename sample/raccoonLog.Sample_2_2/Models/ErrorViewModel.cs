56b9004cce250c7832997dc6da14fab890df9b51
Soheil Alizadeh <xsoheilalizadeh@gmail.com>
2020-01-17 18:40:31 +0330

    Remove samples

--- sample/raccoonLog.Sample_2_2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace raccoonLog.Sample_2_2.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
