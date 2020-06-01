31721596a0b1b1d7c321d0ea375a4f73cc2ccc7c
xsoheilalizadeh <xsoheilalizadeh@gmail.com>
2020-06-01 22:29:50 +0430

    Limit theards to write into log file

--- src/raccoonLog.Stores.File/FileStore.cs
+++ src/raccoonLog.Stores.File/FileStore.cs
@@ -5,14 +5,15 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace raccoonLog.Http.Stores
 {
-    public class FileStore : IHttpLoggingStore, IDisposable
+    public class FileStore : IHttpLoggingStore
     {
         private readonly IHostEnvironment _environment;
 
-        private FileStream _logFileStream;
+        private readonly ILogger<FileStore> _logger;
 
         private IFileSystem _fileSystem;
 
@@ -24,21 +25,24 @@ namespace raccoonLog.Http.Stores
         private static readonly byte[] UTF8StartBracket = { (byte)'[' };
         private static readonly byte[] UTF8Comma = { (byte)',' };
 
+        private static readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
+
         public FileStore(IHostEnvironment environment,
             IOptions<RaccoonLogHttpOptions> logHttpOptions,
             IOptions<FileStoreOptions> options,
             IFileSystem fileSystem
-        )
+, ILogger<FileStore> logger)
         {
             _environment = environment;
             _storeOptions = options.Value;
             _fileSystem = fileSystem;
             _options = logHttpOptions.Value;
+            _logger = logger;
         }
 
         public string DirectoryPath => Path.Combine(_environment.ContentRootPath, _storeOptions.SavePath);
 
-        public async ValueTask StoreAsync(LogContext logContext, CancellationToken cancellationToken = default)
+        public async Task StoreAsync(LogContext logContext, CancellationToken cancellationToken = default)
         {
             if (!Directory.Exists(DirectoryPath))
             {
@@ -47,35 +51,45 @@ namespace raccoonLog.Http.Stores
 
             var filePath = Path.Combine(DirectoryPath, $"{_storeOptions.FileName}");
 
-            _logFileStream = _fileSystem.Open(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            await _semaphoreSlim.WaitAsync();
 
-            if (_logFileStream.Length == 0)
+            try
             {
-                await _logFileStream.WriteAsync(UTF8StartBracket);
+                using var _fileStream = _fileSystem.Open(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+
+                bool isBegin = false;
+
+                if (_fileStream.Length == 0)
+                {
+                    isBegin = true;
+                    await _fileStream.WriteAsync(UTF8StartBracket, cancellationToken);
+                }
+
+                _fileStream.Seek(isBegin ? 0 : -1, SeekOrigin.End);
+
+                var lastCharecter = (char)_fileStream.ReadByte();
+
+                if (lastCharecter == ']')
+                {
+                    _fileStream.Seek(-1, SeekOrigin.End);
+
+                    await _fileStream.WriteAsync(UTF8Comma);
+                }
+
+                var logAsBytes = JsonSerializer.SerializeToUtf8Bytes(logContext, _options.JsonSerializerOptions);
+
+                await _fileStream.WriteAsync(logAsBytes, cancellationToken);
+
+                await _fileStream.WriteAsync(UTF8EndBracket, cancellationToken);
             }
-
-            _logFileStream.Seek(_logFileStream.Length == 0 ? 0 : -1, SeekOrigin.End);
-
-            var lastCharecter = (char)_logFileStream.ReadByte();
-
-            if (lastCharecter == ']')
+            catch (Exception e)
             {
-                _logFileStream.Seek(-1, SeekOrigin.End);
-
-                await _logFileStream.WriteAsync(UTF8Comma);
+                _logger.LogError(e, e.Message);
             }
-
-            var logAsBytes = JsonSerializer.SerializeToUtf8Bytes(logContext, _options.JsonSerializerOptions);
-
-            await _logFileStream.WriteAsync(logAsBytes, cancellationToken);
-
-            await _logFileStream.WriteAsync(UTF8EndBracket);
-        }
-
-        public void Dispose()
-        {
-
-            _logFileStream?.Dispose();
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
     }
 }
