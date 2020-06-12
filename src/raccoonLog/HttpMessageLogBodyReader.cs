27e4cce9868fc6111f2ff803f6abd4e9f5c83924
xsoheilalizadeh <xsoheilalizadeh@gmail.com>
2020-06-12 17:59:30 +0430

    Move body reader to root folder

--- src/raccoonLog/HttpMessageLogBodyReader.cs
+++ src/raccoonLog/HttpMessageLogBodyReader.cs
@@ -6,7 +6,7 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace raccoonLog.Handlers
+namespace raccoonLog
 {
     public class HttpMessageLogBodyReader
     {
